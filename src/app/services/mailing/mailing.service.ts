1ec2e4a9f7eb4c55f0edc362ccfee9a3bd3a0d54
Yuriy Bogatyrenko <yurabogatyrenko@gmail.com>
2017-09-27 18:22:14 +0600

    mailing service added

---
+++ src/app/services/mailing/mailing.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class MailingService {
+
+  constructor() { }
+
+  createMailing() {
+
+  }
+
+  editMailing() {
+
+  }
+
+  stopMailing() {
+
+  }
+
+  getActiveMailing() {
+
+  }
+
+  getActiveMailingList() {
+
+  }
+
+  getMailingArchive() {
+
+  }
+
+  getMailingGirls() {
+
+  }
+
+  getMailingDialogs() {
+
+  }
+
+  addMailingFeedback() {
+
+  }
+}
